8807c65fc3d3332b26c3fbdd0d3ec1453a3c9d77
Wilhelm Teicke <wilhelm.teicke@googlemail.com>
2024-05-23 17:22:50 +0200

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import './App.css'
+import Login from './components/auth/Login'
+import Home from './pages/Home'
+
+
+function App() {
+
+
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/home/" element={<Home />} />
+        </Routes>
+      </Router>
+    </>
+  )
+}
+
+export default App
