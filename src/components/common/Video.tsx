32ecce80cbd5130b893b6cef36dfd30dc5cbfe1f
Wilhelm Teicke <wilhelm.teicke@googlemail.com>
2024-07-16 08:56:30 +0200

    update video480p

--- src/components/common/Video.tsx
+++ src/components/common/Video.tsx
@@ -20,9 +20,7 @@ type Video = {
 
 export default function Video({ video }: VideoProps) {
     const [resolution, setResolution] = useState("original")
-
-    console.log(video)
-
+    const [videoSrc, setVideoSrc] = useState(`${BASE_URL}/${video.video_file}`)
 
     const handleClick = (res: ResProps) => {
         setResolution(res)
@@ -37,30 +35,47 @@ export default function Video({ video }: VideoProps) {
                     headers: {
                         "content-type": "application/json"
                     }
-                })
-                const data = await response.json()
+                });
+                const data = await response.json();
                 console.log(data)
+                console.log(data.video_file_480p)
+                if (data.video_file_480p) {
+                    console.log("source set")
+                    setVideoSrc(`${BASE_URL}/${data.video_file_480p}`);
+                } else {
+                    console.error("480p video URL not found");
+                }
+            } catch (error) {
+                console.error("Error fetching 480p video:", error);
             }
-            catch (error) {
-                console.log(error)
+        };
+
+        if (resolution === "original") {
+            setVideoSrc(`${BASE_URL}/${video.video_file}`);
+        } else if (resolution === "480p") {
+            console.log(video)
+            if (video.video_file_480p) {
+                setVideoSrc(`${BASE_URL}/${video.video_file_480p}`);
+            } else {
+                get480p();
             }
         }
-        if (resolution == "480p") {
-            get480p()
-        }
-    }, [resolution])
+    }, [resolution, video.id, video.video_file, video.video_file_480p]);
 
 
     return (
         <Box key={video.id}>
             <div>{video.title}</div>
             <video width="320" height="240" controls>
-                {resolution == "original" && <source src={`${BASE_URL}/${video.video_file}`} type="video/mp4" />}
-                {resolution == "480p" && <source src={`${BASE_URL}/${video.video_file_480p}`} type="video/mp4" />}
+                {resolution == "original" && <source src={videoSrc} type="video/mp4" />}
                 Your browser does not support the video tag.
             </video>
             <Button variant="contained" onClick={() => handleClick("original")}>Original</Button>
             <Button variant="contained" onClick={() => handleClick("480p")}>480p</Button>
+            <video width="320" height="240" controls>
+                {resolution == "480p" && <source src={`${BASE_URL}/${video.video_file_480p}`} type="video/mp4" />}
+                Your browser does not support the video tag.
+            </video>
         </Box>
     )
 }
